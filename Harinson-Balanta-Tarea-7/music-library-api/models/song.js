289a8f93b66adafe8137d31cbbd96aef02debbad
harri <harrybalanta@gmail.com>
2025-02-02 23:48:36 -0500

    tares 7

---
+++ Harinson-Balanta-Tarea-7/music-library-api/models/song.js
@@ -0,0 +1,23 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Song extends Model {
+    static associate(models) {
+      Song.belongsTo(models.Artist, { foreignKey: 'artistId' });
+    }
+  }
+
+  Song.init({
+    title: DataTypes.STRING,
+    artistId: DataTypes.INTEGER,
+    releaseYear: DataTypes.INTEGER,
+    duration: DataTypes.INTEGER,
+    coverUrl: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Song',
+  });
+
+  return Song;
+};
